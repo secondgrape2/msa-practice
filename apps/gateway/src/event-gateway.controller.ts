86600e7ae9fc8a23f6cfd9bef07b96b0a42bbd6c
grapestore <ikbae0131@gmail.com>
2025-05-19 17:55:33 +0900

    fix: gateway request type error

--- apps/gateway/src/event-gateway.controller.ts
+++ apps/gateway/src/event-gateway.controller.ts
@@ -1,28 +1,26 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  UseGuards,
-  Req,
-  Param,
-  HttpCode,
-  HttpStatus,
-} from '@nestjs/common';
-import { RolesGuard } from '@app/common/auth-core/guards/roles.guard';
-import { Roles } from '@app/common/auth-core/decorators/roles.decorator';
 import { ROLE } from '@app/common/auth-core/constants/role.constants';
+import { Roles } from '@app/common/auth-core/decorators/roles.decorator';
 import { JwtAuthGuard } from '@app/common/auth-core/guards/jwt-auth.guard';
-import { ApiTags, ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { CreateGameEventDto } from '@app/common/event/dto/game-event.dto';
+import { RolesGuard } from '@app/common/auth-core/guards/roles.guard';
+import { AuthenticatedRequest } from '@app/common/auth-core/interfaces/user.interface';
 import {
   GameEventResponseDto,
   GameEventWithRewardsResponseDto,
 } from '@app/common/event/dto/game-event-response.dto';
+import { CreateGameEventDto } from '@app/common/event/dto/game-event.dto';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { GatewayService } from './gateway.service';
-import { AuthenticatedUser } from '@app/common/auth-core/interfaces/user.interface';
-import { ReqUser } from '@app/common/auth-core/decorators/req-user.decorator';
-import { AuthenticatedRequest } from '@app/common/auth-core/interfaces/user.interface';
 
 @ApiTags('Events')
 @Controller('api/events/v1')
@@ -37,7 +35,7 @@ export class EventGatewayController {
   @HttpCode(HttpStatus.CREATED)
   async createEvent(
     @Body() createEventDto: CreateGameEventDto,
-    @ReqUser() user: AuthenticatedUser,
+    @Req() req: AuthenticatedRequest,
   ) {
     return this.gatewayService.proxyToEventService<GameEventResponseDto>(
       '/events/v1/admin',
